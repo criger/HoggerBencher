267b0d342258e4e0b35a0599e5ca465d932ab072
Cristhian Gertner <criger@gmail.com>
2025-02-25 00:46:18 +0100

    string test completed
    added string test with GC, allocated memory and how much has been used.

--- StringVsStringBuilder.xaml.cs
+++ StringVsStringBuilder.xaml.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 
 namespace HoggerBencher
@@ -30,18 +33,51 @@ namespace HoggerBencher
         private async void StartStringVsStringBuilderTestBtn_Click(object sender, RoutedEventArgs e)
         {
 
-            try
+            var gcMemoryInfo = GC.GetGCMemoryInfo();
+            var installedMemoryBytes = gcMemoryInfo.TotalAvailableMemoryBytes;  // / 1024;
+            var usedMemoryBytes = GC.GetTotalMemory(true); // / 1024;
+            var availableMemoryBytes = installedMemoryBytes - usedMemoryBytes;
+
+            var bytesToAllocate = availableMemoryBytes / 4;
+
+            if (GC.TryStartNoGCRegion(bytesToAllocate, true))
             {
-                var numLoops = Int32.Parse(numOfLoopsInput.Text);
+                try
+                {
+                    var numLoops = Int32.Parse(numOfLoopsInput.Text);
 
-                string stringTest = await TestString.TestMemory(numLoops, null);
+                    long beforeStringTest = Process.GetCurrentProcess().WorkingSet64;
+                    string stringTest = await TestString.TestMemory(numLoops, null);
+                    long afterStringTest = Process.GetCurrentProcess().WorkingSet64;
 
-                stringVsStringBuilderResult.Text = stringTest;
+                    long stringTestUsedMem = afterStringTest - beforeStringTest;
 
-            }
-            catch (FormatException fe)
-            {
-                MessageBox.Show("Please type a number in the textbox...");
+                    stringVsStringBuilderResult.Text = "String test wrote XXXX " + numLoops + " times with a newline after each iteration." + System.Environment.NewLine
+                                                     + "Further on, the String test consumed " + stringTestUsedMem + " bytes (" + stringTestUsedMem / 1000 + " KB or " + stringTestUsedMem / 1000000 + " MB...!)";
+
+                    stringVsStringBuilderResult.Text += System.Environment.NewLine;
+                    stringVsStringBuilderResult.Text += System.Environment.NewLine;
+                    stringVsStringBuilderResult.Text += "Be aware that the numbers may not be 100% accurate.";
+                    stringVsStringBuilderResult.Text += System.Environment.NewLine;
+                    stringVsStringBuilderResult.Text += "Prior to running the test, Windows Garbage Collection is instructed to 'save' " + bytesToAllocate + " bytes of the memory from being collected and released.";
+                    stringVsStringBuilderResult.Text += System.Environment.NewLine;
+                    stringVsStringBuilderResult.Text += "However, it is NOT a guarantee that Garbace Collection did NOT run.. (thanks Microsoft...!)";
+                    stringVsStringBuilderResult.Text += System.Environment.NewLine;
+                    stringVsStringBuilderResult.Text += "Therefore, it is advised to run the test some more times in order to get a complete overview of how much memory has been used.";
+
+
+                }
+                catch (FormatException fe)
+                {
+                    MessageBox.Show("Please type a number in the textbox...");
+
+                }
+
+                finally
+                {
+                    if (GCSettings.LatencyMode == GCLatencyMode.NoGCRegion)
+                        GC.EndNoGCRegion();
+                }
 
             }
 
