a8415f9e260108b4d48bcf13dea9bc780294b3c6
Cristhian Gertner <criger@gmail.com>
2025-03-24 00:44:15 +0100

    Initial GUI for binary madness

---
+++ BinaryMadness.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HoggerBencher
+{
+    /// <summary>
+    /// Interaction logic for BinaryMadness.xaml
+    /// </summary>
+    public partial class BinaryMadness : Window
+    {
+        public BinaryMadness()
+        {
+            InitializeComponent();
+        }
+
+        private void findNumberBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
